f0ac19d07d764478a9c210f9800565f577cc8fa4
ivenckunas <ignas.venckunas@gmail.com>
2024-02-15 10:25:00 +0200

    added mobile burger menu

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,18 +2,45 @@ import styles from './nav.module.scss';
 import Image from 'next/image';
 import logo from '../../assets/logo.svg';
 import menu from '../../assets/menu.svg';
+import close from '../../assets/close.svg';
+import {useState} from 'react';
 
 export default function Nav() {
+	const [showBurger, setShowBurger] = useState(false);
+
+	const handleBurgerClick = () => {
+		setShowBurger(!showBurger);
+	};
+
 	return (
 		<div className={styles.container}>
 			<Image
 				src={logo}
-				alt='Picture of the author'
+				alt='RatePunk logo'
 			/>
 			<Image
+				onClick={handleBurgerClick}
 				src={menu}
-				alt='Picture of the author'
+				alt='burger menu'
 			/>
+			<div className={showBurger ? styles.menuShow : styles.menuHide}>
+				<div className={styles.menuHeader}>
+					<Image
+						src={logo}
+						alt='RatePunk logo'
+					/>
+					<Image
+						onClick={() => setShowBurger(false)}
+						src={close}
+						alt='RatePunk logo'
+					/>
+				</div>
+				<ul>
+					<li>Chrome extension</li>
+					<li>Price Comparison</li>
+					<li>Blog</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
